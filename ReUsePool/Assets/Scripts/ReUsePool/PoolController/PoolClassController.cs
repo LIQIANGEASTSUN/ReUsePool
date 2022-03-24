15889eb0bd9cf7956f30b904163c8997dcb6cbc1
LQ <liqiangeastsun@163.com>
2022-03-24 17:27:05 +0800

    no message

--- ReUsePool/Assets/Scripts/ReUsePool/PoolController/PoolClassController.cs
+++ ReUsePool/Assets/Scripts/ReUsePool/PoolController/PoolClassController.cs
@@ -9,64 +9,6 @@ namespace ReUsePool
     /// 类复用池管理：类复用需要继承 IReUse 接口
     /// 一个类创建一个池子， typeof(类).Name 作为池子的名字
     /// </summary>
-    //public class PoolClassController : IPool<IReUse>
-    //{
-    //    private Dictionary<string, Pool<IReUse>> _poolDic = new Dictionary<string, Pool<IReUse>>();
-
-    //    public IReUse Spawn(string poolName) 
-    //    {
-    //        Pool<IReUse> pool = GetPool(poolName);
-    //        if (null == pool)
-    //        {
-    //            return null;
-    //        }
-    //        return pool.Spawn(string.Empty);
-    //    }
-
-    //    public void UnSpawn(string poolName, IReUse t)
-    //    {
-    //        Pool<IReUse> pool = GetPool(t);
-    //        if (null == pool)
-    //        {
-    //            pool = CreatePool(t);
-    //        }
-    //        pool.UnSpawn(string.Empty, t);
-    //    }
-
-    //    public void Release(IReUse t)
-    //    {
-
-    //    }
-
-    //    private Pool<IReUse> CreatePool(IReUse t)
-    //    {
-    //        string typeName = TypeName(t);
-    //        Pool<IReUse> pool = new PoolClass<IReUse>();
-    //        Debug.LogError("CreatePool:" + typeName);
-    //        _poolDic.Add(typeName, pool);
-    //        return pool;
-    //    }
-
-    //    private Pool<IReUse> GetPool(IReUse t)
-    //    {
-    //        string typeName = TypeName(t);
-    //        return GetPool(typeName);
-    //    }
-
-    //    private Pool<IReUse> GetPool(string poolName)
-    //    {
-    //        Pool<IReUse> pool = null;
-    //        _poolDic.TryGetValue(poolName, out pool);
-    //        return pool;
-    //    }
-
-    //    private string TypeName(IReUse t)
-    //    {
-    //        return t.GetType().Name;
-    //    }
-
-    //}
-
     public class PoolClassController : PoolControllerBase<IReUse>
     {
 
@@ -95,3 +37,61 @@ namespace ReUsePool
     }
 
 }
+
+//public class PoolClassController : IPool<IReUse>
+//{
+//    private Dictionary<string, Pool<IReUse>> _poolDic = new Dictionary<string, Pool<IReUse>>();
+
+//    public IReUse Spawn(string poolName) 
+//    {
+//        Pool<IReUse> pool = GetPool(poolName);
+//        if (null == pool)
+//        {
+//            return null;
+//        }
+//        return pool.Spawn(string.Empty);
+//    }
+
+//    public void UnSpawn(string poolName, IReUse t)
+//    {
+//        Pool<IReUse> pool = GetPool(t);
+//        if (null == pool)
+//        {
+//            pool = CreatePool(t);
+//        }
+//        pool.UnSpawn(string.Empty, t);
+//    }
+
+//    public void Release(IReUse t)
+//    {
+
+//    }
+
+//    private Pool<IReUse> CreatePool(IReUse t)
+//    {
+//        string typeName = TypeName(t);
+//        Pool<IReUse> pool = new PoolClass<IReUse>();
+//        Debug.LogError("CreatePool:" + typeName);
+//        _poolDic.Add(typeName, pool);
+//        return pool;
+//    }
+
+//    private Pool<IReUse> GetPool(IReUse t)
+//    {
+//        string typeName = TypeName(t);
+//        return GetPool(typeName);
+//    }
+
+//    private Pool<IReUse> GetPool(string poolName)
+//    {
+//        Pool<IReUse> pool = null;
+//        _poolDic.TryGetValue(poolName, out pool);
+//        return pool;
+//    }
+
+//    private string TypeName(IReUse t)
+//    {
+//        return t.GetType().Name;
+//    }
+
+//}
--- ReUsePool/Assets/Scripts/ReUsePool/PoolController/PoolObjectController.cs
+++ ReUsePool/Assets/Scripts/ReUsePool/PoolController/PoolObjectController.cs
@@ -10,51 +10,6 @@ namespace ReUsePool
     /// 一个资源名创建一个池子， 资源名作为池子的名字
     /// 注意：不要有重复名字的资源，如果不同资源名字重复了，一定是项目架构中的命名规范有问题
     /// </summary>
-    //public class PoolObjectController : IPool<UnityEngine.GameObject>
-    //{
-    //    private Dictionary<string, Pool<UnityEngine.GameObject>> _poolDic = new Dictionary<string, Pool<UnityEngine.GameObject>>();
-
-    //    public UnityEngine.GameObject Spawn(string poolName)
-    //    {
-    //        Pool<UnityEngine.GameObject> pool = GetPool(poolName);
-    //        if (null == pool)
-    //        {
-    //            return null;
-    //        }
-    //        return pool.Spawn(poolName);
-    //    }
-
-    //    public void UnSpawn(string poolName, UnityEngine.GameObject t)
-    //    {
-    //        Pool<UnityEngine.GameObject> pool = GetPool(poolName);
-    //        if (null == pool)
-    //        {
-    //            pool = CreatePool(poolName);
-    //        }
-    //        pool.UnSpawn(poolName, t);
-    //    }
-
-    //    public void Release(UnityEngine.GameObject t)
-    //    {
-
-    //    }
-
-    //    private Pool<UnityEngine.GameObject> CreatePool(string poolName)
-    //    {
-    //        Pool<UnityEngine.GameObject> pool = new PoolGo<UnityEngine.GameObject>();
-    //        Debug.LogError("CreatePool:" + poolName);
-    //        _poolDic.Add(poolName, pool);
-    //        return pool;
-    //    }
-
-    //    private Pool<UnityEngine.GameObject> GetPool(string poolName)
-    //    {
-    //        Pool<UnityEngine.GameObject> pool = null;
-    //        _poolDic.TryGetValue(poolName, out pool);
-    //        return pool;
-    //    }
-    //}
-
     public class PoolObjectController : PoolControllerBase<UnityEngine.GameObject>
     {
         public override UnityEngine.GameObject Spawn(string poolName)
@@ -82,3 +37,49 @@ namespace ReUsePool
     }
 
 }
+
+
+//public class PoolObjectController : IPool<UnityEngine.GameObject>
+//{
+//    private Dictionary<string, Pool<UnityEngine.GameObject>> _poolDic = new Dictionary<string, Pool<UnityEngine.GameObject>>();
+
+//    public UnityEngine.GameObject Spawn(string poolName)
+//    {
+//        Pool<UnityEngine.GameObject> pool = GetPool(poolName);
+//        if (null == pool)
+//        {
+//            return null;
+//        }
+//        return pool.Spawn(poolName);
+//    }
+
+//    public void UnSpawn(string poolName, UnityEngine.GameObject t)
+//    {
+//        Pool<UnityEngine.GameObject> pool = GetPool(poolName);
+//        if (null == pool)
+//        {
+//            pool = CreatePool(poolName);
+//        }
+//        pool.UnSpawn(poolName, t);
+//    }
+
+//    public void Release(UnityEngine.GameObject t)
+//    {
+
+//    }
+
+//    private Pool<UnityEngine.GameObject> CreatePool(string poolName)
+//    {
+//        Pool<UnityEngine.GameObject> pool = new PoolGo<UnityEngine.GameObject>();
+//        Debug.LogError("CreatePool:" + poolName);
+//        _poolDic.Add(poolName, pool);
+//        return pool;
+//    }
+
+//    private Pool<UnityEngine.GameObject> GetPool(string poolName)
+//    {
+//        Pool<UnityEngine.GameObject> pool = null;
+//        _poolDic.TryGetValue(poolName, out pool);
+//        return pool;
+//    }
+//}
